36f006b0c5917607026de04bb30724e3db1d2e2f
snowapril <sinjihng@naver.com>
2021-04-06 00:07:13 +0900

    feat : modify BaseGeneratorTests directory path

--- Tests/UnitTests/BaseGeneratorTests.cpp
+++ Tests/UnitTests/BaseGeneratorTests.cpp
@@ -5,5 +5,5 @@
 TEST_CASE("BaseGenerator test")
 {
     Generator::BaseGenerator testSet;
-    CHECK(testSet.LoadDataSet(DATASETS_DIR "3d10000.csv") == true);
+    CHECK(testSet.LoadDataSet(DATASETS_DIR "/3d10000.csv") == true);
 }
