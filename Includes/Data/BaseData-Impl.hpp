2fa5e859d11427401482b5abb96a3470db88f92b
snowapril <sinjihng@naver.com>
2021-04-05 23:08:00 +0900

    feat : Implement BaseData as data vector
    implement several arithmetic operator overloading

--- Includes/Data/BaseData-Impl.hpp
+++ Includes/Data/BaseData-Impl.hpp
@@ -7,6 +7,126 @@
 namespace Data
 {
 	template <typename Type>
+	BaseData<Type>::BaseData(size_t num, AttributeType initialValue)
+	{
+		Resize(num, initialValue);
+	}
+
+	template <typename Type>
+	BaseData<Type>::BaseData(const std::initializer_list<AttributeType>& list)
+	{
+		Set(list);
+	}
+
+	template <typename Type>
+	typename BaseData<Type>::AttributeType& BaseData<Type>::operator[](size_t i)
+	{
+		return _attributes[i];
+	}
+
+	template <typename Type>
+	const typename BaseData<Type>::AttributeType& BaseData<Type>::operator[](size_t i) const
+	{
+		return _attributes[i];
+	}
+
+	template <typename Type>
+	typename BaseData<Type>::AttributeType& BaseData<Type>::operator()(size_t i)
+	{
+		return _attributes[i];
+	}
+
+	template <typename Type>
+	const typename BaseData<Type>::AttributeType& BaseData<Type>::operator()(size_t i) const
+	{
+		return _attributes[i];
+	}
+
+	template <typename Type>
+	void BaseData<Type>::Resize(size_t num, AttributeType initialValue)
+	{
+		_attributes.resize(num, initialValue);
+	}
+
+	template <typename Type>
+	void BaseData<Type>::Set(const std::initializer_list<AttributeType>& list)
+	{
+		_attributes.assign(list);
+	}
+
+	template <typename Type>
+	const BaseData<Type> BaseData<Type>::operator+(const AttributeType& scalar)
+	{
+		BaseData<Type> data(*this);
+		data += scalar;
+		return data;
+	}
+
+	template <typename Type>
+	const BaseData<Type> BaseData<Type>::operator-(const AttributeType& scalar)
+	{
+		BaseData<Type> data(*this);
+		data -= scalar;
+		return data;
+	}
+
+	template <typename Type>
+	const BaseData<Type> BaseData<Type>::operator*(const AttributeType& scalar)
+	{
+		BaseData<Type> data(*this);
+		data *= scalar;
+		return data;
+	}
+
+	template <typename Type>
+	const BaseData<Type> BaseData<Type>::operator/(const AttributeType& scalar)
+	{
+		BaseData<Type> data(*this);
+		data /= scalar;
+		return data;
+	}
+
+	template <typename Type>
+	BaseData<Type>& BaseData<Type>::operator+=(const AttributeType& scalar)
+	{
+		for (auto& value : _attributes)
+			value += scalar;
+		return *this;
+	}
+
+	template <typename Type>
+	BaseData<Type>& BaseData<Type>::operator-=(const AttributeType& scalar)
+	{
+		for (auto& value : _attributes)
+			value -= scalar;
+		return *this;
+	}
+
+	template <typename Type>
+	BaseData<Type>& BaseData<Type>::operator*=(const AttributeType& scalar)
+	{
+		for (auto& value : _attributes)
+			value *= scalar;
+		return *this;
+	}
+
+	template <typename Type>
+	BaseData<Type>& BaseData<Type>::operator/=(const AttributeType& scalar)
+	{
+		constexpr Type kEpsilon = static_cast<Type>(1e-9);
+		for (auto& value : _attributes)
+			value /= (scalar + kEpsilon);
+		return *this;
+	}
+
+
+	template <typename Type>
+	size_t BaseData<Type>::Size() const
+	{
+		return _attributes.size();
+	}
+
+	template <typename Type>
 	typename BaseData<Type>::Iterator BaseData<Type>::begin()
 	{
 		return _attributes.begin();
--- Includes/Data/BaseData.hpp
+++ Includes/Data/BaseData.hpp
@@ -18,13 +18,70 @@ namespace Data
 	class BaseData
 	{
 	public:
-		using ContainerType = std::vector<Type>;
+		using AttributeType = typename Type;
+		using ContainerType = std::vector<AttributeType>;
 		using Iterator = typename ContainerType::iterator;
 		using ConstIterator = typename ContainerType::const_iterator;
 		//! Default constructor
 		BaseData() = default;
+		//! Generate the base data with give number of attributes
+		BaseData(size_t num, AttributeType initialValue = 0);
+		//!
+		//! \brief constructs a base data with initializer list \p list
+		//! 
+		//! This constructpr will build a base data with given initializer list \p list
+		//! such as
+		//! 
+		//! \code{.cpp}
+		//! BaseData<float> mat = {1.0f, 2.0f, 3.0f, 4.0f};
+		//! \endcode
+		//! 
+		//! \param list - initializer list that should be copy to the new base data.
+		//! 
+		BaseData(const std::initializer_list<AttributeType>& list);
 		//! Default destructor
 		virtual ~BaseData() = default;
+		//! Returns reference of i-th element
+		AttributeType& operator[](size_t i);
+		//! Returns const reference of i-th element
+		const AttributeType& operator[](size_t i) const;
+		//! Returns reference of i-th element
+		AttributeType& operator()(size_t i);
+		//! Returns const reference of i-th element
+		const AttributeType& operator()(size_t i) const;
+		//! Resize the number of attributes
+		void Resize(size_t num, AttributeType initialValue = 0);
+		//!
+		//! \brief constructs a base data with initializer list \p list
+		//! 
+		//! This constructpr will build a base data with given initializer list \p list
+		//! such as
+		//! 
+		//! \code{.cpp}
+		//! BaseData<float> mat = {1.0f, 2.0f, 3.0f, 4.0f};
+		//! \endcode
+		//! 
+		//! \param list - initializer list that should be copy to the new base data.
+		//! 
+		void Set(const std::initializer_list<AttributeType>& list);
+		//! Returns new data row + input scalar
+		const BaseData<Type> operator+(const AttributeType& scalar);
+		//! Returns new data row - input scalar
+		const BaseData<Type> operator-(const AttributeType& scalar);
+		//! Returns new data row * input scalar
+		const BaseData<Type> operator*(const AttributeType& scalar);
+		//! Returns new data row / input scalar
+		const BaseData<Type> operator/(const AttributeType& scalar);
+		//! Returns this data row + input scalar
+		BaseData<Type>& operator+=(const AttributeType& scalar);
+		//! Returns this data row - input scalar
+		BaseData<Type>& operator-=(const AttributeType& scalar);
+		//! Returns this data row * input scalar
+		BaseData<Type>& operator*=(const AttributeType& scalar);
+		//! Returns this data row / input scalar
+		BaseData<Type>& operator/=(const AttributeType& scalar);
+		//! Returns the total number of elements in this data
+		size_t Size() const;
 		//! Return iterator of the begin point.
 		Iterator begin();
 		//! Return iteartor of the end point
