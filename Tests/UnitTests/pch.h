5df2745a90137901e9c5f0b7589f6da5b8b94b40
snowapril <sinjihng@naver.com>
2021-04-05 23:05:31 +0900

    feat : add utility macro for checking equality with & without epsilon

--- Tests/UnitTests/pch.h
+++ Tests/UnitTests/pch.h
@@ -6,4 +6,8 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 #include <doctest/doctest.h>
+
+#define CHECK_EQUAL(v1, v2) CHECK((v1) == (v2))
+#define CHECK_EQUAL_EPSILON(v1, v2, eps) CHECK(std::fabs((v1) - (v2)) <= eps)
