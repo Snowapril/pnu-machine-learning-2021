0ffbaf16bba4e40bc385782c8c363f5ea890a241
snowapril <sinjihng@naver.com>
2021-04-07 00:15:28 +0900

    feat : add weight matrix initializer util functions
    Constant initializer init weight values as given constant argument. 
    Xavier uniform initializer not implemented yet, init weight matrix with xavier uniform distribution

---
+++ Includes/Learner/Initializer.hpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2021, Jihong Shin
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef INITIALIZER_HPP
+#define INITIALIZER_HPP
+
+#include <Math/Matrix.hpp>
+#include <type_traits>
+#include <functional>
+
+namespace Learner
+{
+	namespace Initializer
+	{
+		//!
+		//! \brief initialize weight matrix with constant value 
+		//! \tparam Type - weight matrix data type
+		//! \param weight - target weight matrix
+		//! \param constant - constant value for initialized weight value
+		//! 
+		template <typename Type, typename = std::enable_if_t<std::is_floating_point_v<Type>>>
+		void Constant(Math::Matrix<Type>& weight, const Type& constant)
+		{
+			for (size_t i = 0; i < weight.Col(); ++i)
+				for (size_t j = 0; j < weight.Row(); ++j)
+					weight[i][j] = constant;
+		}
+
+		//!
+		//! \brief initialize weight matrix with xavier uniform distribution
+		//! \tparam Type - weight matrix data type
+		//! \param weight - target weight matrix
+		//! \param constant - xavier uniform distribution parameter
+		//! 
+		template <typename Type, typename = std::enable_if_t<std::is_floating_point_v<Type>>>
+		void XavierUniform(Math::Matrix<Type>& weight, const Type& parameter)
+		{
+			// TODO(snowapril) : must implement xavier uniform initialization.
+			
+			for (size_t i = 0; i < weight.Col(); ++i)
+				for (size_t j = 0; j < weight.Row(); ++j)
+					weight[i][j] = parameter;
+		}
+	};
+};
+
+#endif //! end of Activation.hpp
