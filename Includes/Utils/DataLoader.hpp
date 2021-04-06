d292a59b8c399b1103f09f2ce3a9cadd616f8ae3
snowapril <sinjihng@naver.com>
2021-04-07 00:30:22 +0900

    feat : add csv format file parsing function
    parsing its contents to string vector and passing it to given vector pointer argument

--- Includes/Utils/DataLoader.hpp
+++ Includes/Utils/DataLoader.hpp
@@ -15,7 +15,10 @@ namespace Utils
 	class DataLoader
 	{
 	public:
+		//! load file and pass the contents to given vector argument.
 		static bool LoadFile(const std::string& filename, std::vector<char>* contents, bool binaryFlag = false);
+		//! interpret the contents as CSV format and parse string contents
+		static bool ParseContentsFromCSV(const std::vector<char>& contents, std::vector<std::vector<std::string>>* parsedContents, bool exceptHeader);
 	};
 };
 
--- Sources/Utils/DataLoader.cpp
+++ Sources/Utils/DataLoader.cpp
@@ -7,6 +7,7 @@
 #include <Utils/DataLoader.hpp>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 namespace Utils
 {
@@ -28,6 +29,29 @@ namespace Utils
 		file.read(contents->data(), fileSize);
 		
 		file.close();
+		return true;
+	}
+
+	bool DataLoader::ParseContentsFromCSV(const std::vector<char>& contents, std::vector<std::vector<std::string>>* parsedContents, bool exceptHeader)
+	{
+		std::istringstream isstr(std::string(contents.begin(), contents.end()));
+		std::string temp;
+
+		//! Remove first line of the csv file.
+		if (exceptHeader) 
+			std::getline(isstr, temp);
+
+		//! Read one line from the csv file.
+		while (std::getline(isstr, temp))
+		{
+			std::istringstream valueParser(temp);
+			std::vector<std::string> datum;
+			//! Split the string stripped with ','
+			while (std::getline(valueParser, temp, ','))
+				datum.push_back(temp);
+			parsedContents->emplace_back(std::move(datum));
+		}
+
 		return true;
 	}
 };
