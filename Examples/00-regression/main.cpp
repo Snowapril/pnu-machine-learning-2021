5e5747e52495bfcea3b5610a0f7ddff7eeaeb6c6
snowapril <sinjihng@naver.com>
2021-04-07 00:31:49 +0900

    feat : implement simple regression learning ongoing

--- Examples/00-regression/main.cpp
+++ Examples/00-regression/main.cpp
@@ -7,19 +7,25 @@
 #include <iostream>
 #include <Generator/BaseGenerator.hpp>
 #include <Learner/Regression.hpp>
+#include <Learner/Initializer.hpp>
+#include <memory>
 
 int main(int argc, char* argv[])
 {
 	(void)argc; (void)argv;
-	Learner::Regression regressionLearner;
 
-	Generator::BaseGenerator trainSet, testSet;
-	trainSet.LoadDataSet(DATASETS_DIR "/3d100a.csv");
-	testSet .LoadDataSet(DATASETS_DIR "/3d100b.csv");
+	const std::initializer_list<size_t> targetIndices = { 3 };
+	auto trainSet = std::make_shared<Generator::BaseGenerator>(DATASETS_DIR "/3d100a.csv", targetIndices);
+	auto testSet  = std::make_shared<Generator::BaseGenerator>(DATASETS_DIR "/3d100b.csv", targetIndices);
+
+	Learner::Regression regressionLearner;
+	regressionLearner.AddDenseLayer<Learner::Activation::Sigmoid<float>>(4, 1, [](Math::Matrix<float>& weights) {
+		Learner::Initializer::XavierUniform(weights, 0.0f);
+	});
 
 	regressionLearner.TrainOnBatch(trainSet);
 
-	std::vector<Data::StructuredDataF32> prediction;
+	std::vector<Math::FMatrix> prediction;
 	regressionLearner.PredictOnBatch(testSet, &prediction);
 	
 	return 0;
