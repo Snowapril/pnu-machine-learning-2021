06ec20d4aa2664966d4e9e2b136259e38157fc01
snowapril <sinjihng@naver.com>
2021-04-07 13:20:02 +0900

    feat : add model summarizing output printing method

--- Sources/Learner/Regression.cpp
+++ Sources/Learner/Regression.cpp
@@ -5,12 +5,29 @@
 // property of any third parties.
 
 #include <Learner/Regression.hpp>
+#include <iostream>
 
 namespace Learner
 {
-	//! Set learning rate handler function
 	void Regression::SetLearningRateHandler(LearningRate::LearningRateFunc<float> handler)
 	{
 		_lrHandler = handler;
+	}
+
+	void Regression::AddUnitLayer(const std::shared_ptr<Layer::BaseLayer<float>>& layer)
+	{
+		_layers.push_back(layer);
+	}
+
+	void Regression::Summarize() const
+	{
+		std::cout << "=====================Model Summary=====================" << std::endl;
+		std::cout << "Layer\t\t\tShape\t\t\t#Param" << std::endl;
+		std::cout << "-------------------------------------------------------" << std::endl;
+		for (const auto& layer : _layers)
+		{
+			layer->VerbosePrint();
+			std::cout << "-------------------------------------------------------" << std::endl;
+		}
 	}
 };
