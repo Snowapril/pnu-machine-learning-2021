b5ffc56be29f5a18c4717e640c6368b64653472f
snowapril <sinjihng@naver.com>
2021-04-08 20:15:21 +0900

    feat : cross fold generator will generate BatchGenerator not data batch

--- Includes/Generator/CrossFoldGenerator.hpp
+++ Includes/Generator/CrossFoldGenerator.hpp
@@ -7,27 +7,15 @@
 #ifndef CROSS_FOLD_GENERATOR_HPP
 #define CROSS_FOLD_GENERATOR_HPP
 
-#include <Generator/BaseGenerator.hpp>
-
 namespace Generator
 {
-	class CrossFoldGenerator : public BaseGenerator
+	class CrossFoldGenerator
 	{
 	public:
-		using DataType = Math::FMatrix;
-		using ContainerType = std::vector<DataType>;
-		using Iterator = typename ContainerType::iterator;
-		using ConstIterator = typename ContainerType::const_iterator;
-		using BatchType = std::pair<ConstIterator, ConstIterator>;
 		//! Default constructor
 		CrossFoldGenerator() = default;
 		//! Default destructor
 		~CrossFoldGenerator() = default;
-		//! Generate begin, end point pair of the input dataset vector in one epoch
-		BatchType GetInputBatch(size_t epochIdx) override;
-		//! Generate begin, end point pair of the label dataset vector in one epoch
-		BatchType GetLabelBatch(size_t epochIdx) override;
-	protected:
 	private:
 	};
 };
