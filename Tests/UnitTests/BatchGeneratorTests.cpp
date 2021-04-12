e44d204846929954740b7527e5fbab7fefc57068
snowapril <sinjihng@naver.com>
2021-04-12 21:16:20 +0900

    feat : switch project language to python from c++
    I decided to change language from c++ to python. There is a few reason
    that why i decided to change language.
    1. there is several useful modules such as numpy and pandas, etc..
    2. i have no time for completing before assignment submition due.

--- Tests/UnitTests/BatchGeneratorTests.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-#include <Generator/BatchGenerator.hpp>
-
-TEST_CASE("BatchGenerator test")
-{
-    Generator::BatchGenerator testSet;
-    CHECK(testSet.LoadDataSet(DATASETS_DIR "/3d10000.csv", { 3 }) == true);
-}
