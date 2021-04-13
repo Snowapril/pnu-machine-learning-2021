bba2410fece99aded2b118cb1ea98de3d250cb87
snowapril <sinjihng@naver.com>
2021-04-13 16:35:05 +0900

    feat(refactor) : modify codestyle followed by PEP8

--- pnuml/visualization/plotting.py
+++ pnuml/visualization/plotting.py
@@ -10,6 +10,7 @@ def visualize_loss(loss, save_path=None):
     plt.plot(x, loss)
     plt.show()
 
+
 def roc_curve(tp, fp, save_path=None):
     """
     visualize ROC Curve with given True positive
